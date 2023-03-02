9657e1922a4b63c1fc24aa66dbdc3f3dcc15d499
Ivan <lardius@mail.ru>
2023-03-02 12:41:23 +0300

    Seminar_1

---
+++ Examples_task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 1){
+    System.Console.WriteLine("Число " + a + " не чётное");
+} else {
+    System.Console.WriteLine("Число " + a + " чётное");
+}
---
+++ Examples_task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+String p = "";
+while (b < a) {
+    if(p == "") {
+        p = p + b;
+    } else {
+        p = p  + ", " + b;  
+    }
+    
+    b = b + 2;
+    
+}
+System.Console.WriteLine("Все чётные числа: " + p);
