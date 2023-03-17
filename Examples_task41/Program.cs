bfe6234783cdc1327dc29a01174e6fcc6f155c83
Ivan <lardius@mail.ru>
2023-03-17 10:20:06 +0300

    Seminar_6

---
+++ Examples_task41/Program.cs
@@ -0,0 +1,28 @@
+﻿string Prompt(string intro, bool oneline = true)
+{
+    Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
+    string res = Console.ReadLine() ?? "";
+    return res;
+}
+int[] ConvertStringToIntArray(string s, string splitString = " ")
+{
+    return s.Split(splitString).Select(item => int.Parse(item)).ToArray();
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+int CountMoreZiro(string s)
+{
+    int[] array = ConvertStringToIntArray(s);
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    }
+    return count;
+}
+
+string s = Prompt("Введите любое колво цифр разделенных пробелами:");
+PrintArray(ConvertStringToIntArray(s));
+Console.WriteLine(CountMoreZiro(s));
---
+++ Examples_task43/Program.cs
@@ -0,0 +1,57 @@
+﻿int X = 0; 
+int Y = 1; 
+int K = 0; 
+int B = 1; 
+
+string Prompt(string intro, bool oneline = true)
+{
+    Console.Write($"{intro}" + ((oneline) ? "" : "\n").ToString());
+    string res = Console.ReadLine() ?? "";
+    return res;
+}
+
+double?[] GetCrossCoordinate(double[] first, double[] second)
+{
+    double?[] res = new double?[2];
+    if (first[K] == second[K])
+    {
+        res[X] = null;
+        res[Y] = (first[B] == second[B]) ? null : 0;  
+    }
+    else
+    {
+        res[X] = (first[B] - second[B]) / (second[K] - first[K]);
+        res[Y] = first[K] * res[X] + first[B];
+    }
+    return res;
+}
+
+double[] firstLine = Prompt("Введите первое линейное уравнение вида y=kx+b -> у=")
+                    .ToLower()                          
+                    .Replace("х", "x")                  
+                    .Split("x")                         
+                    .Select(item => double.Parse(item)) 
+                    .ToArray();                         
+double[] secondLine = Prompt("Введите второе линейное уравнение вида y=kx+b -> у=")
+                    .ToLower()
+                    .Replace("х", "x")
+                    .Split("x")
+                    .Select(item => double.Parse(item))
+                    .ToArray();
+double?[] crossCoordinate = GetCrossCoordinate(firstLine, secondLine);
+
+if (crossCoordinate[X] != null)
+{
+    Console.WriteLine($"({crossCoordinate[X]}; {crossCoordinate[Y]})");
+}
+else
+{
+    if (crossCoordinate[Y] == null)
+    {
+        Console.WriteLine("Прямые совпадают.");
+    }
+    else
+    {
+        Console.WriteLine("Прямые паралельны.");
+    }
+}
